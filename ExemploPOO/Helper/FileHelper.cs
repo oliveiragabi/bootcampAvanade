24afe1614c15278134f68872ae9ef81321799fa9
Gabrielle Souza <gabikazsouza@gmail.com>
2021-12-25 00:45:32 -0300

    subindo exemplo lista de diretorios - Feliz Natal, hohoho

--- ExemploPOO/Helper/FileHelper.cs
+++ ExemploPOO/Helper/FileHelper.cs
@@ -2,11 +2,11 @@ using System.IO;
 
 namespace ExemploPOO.Helper
 {
-    public class NewBaseType
+    public class FileHelper
     {
         public void ListarDiretorios(string caminho)
         {
-            var retornoCaminho = Directory.GetDirectories(caminho);
+            var retornoCaminho = Directory.GetDirectories(caminho, "*", SearchOption.AllDirectories);
 
             foreach (var retorno in retornoCaminho)
             {
