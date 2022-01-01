3a0b73a6bb1f8e123f83e087d1e54bc24efe229f
Gabrielle Souza <gabikazsouza@gmail.com>
2022-01-01 20:09:19 -0300

    exemplo percorrendo matriz

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -6,18 +6,33 @@ namespace Colecoes
     class Program
     {
         static void Main(string[] args)
-        {        
-            int[] arrayInt = new int[3];
-            arrayInt[0] = 10;
-            arrayInt[1] = 5;
-            arrayInt[2] = 6;
-
-            System.Console.WriteLine("Percorrendo array");
-            
-            for (int i = 0; i < arrayInt.Length; i++)
+        {   
+            int[,] matriz = new int [4, 2]
             {
-                System.Console.WriteLine(arrayInt[i]);
-            }
+                { 8 , 8 },
+                { 10, 20 },
+                { 50, 100 },
+                { 90, 200 }
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    System.Console.WriteLine(matriz[i, j]);
+                }
+            }     
+            // int[] arrayInt = new int[3];
+            // arrayInt[0] = 10;
+            // arrayInt[1] = 5;
+            // arrayInt[2] = 6;
+
+            // System.Console.WriteLine("Percorrendo array");
+            
+            // for (int i = 0; i < arrayInt.Length; i++)
+            // {
+            //     System.Console.WriteLine(arrayInt[i]);
+            // }
         }
          
     }
