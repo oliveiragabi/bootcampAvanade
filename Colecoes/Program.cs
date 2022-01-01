59a01dea9da5d6ebf0ee64f81296e5410069b226
Gabrielle Souza <gabikazsouza@gmail.com>
2022-01-01 18:08:55 -0300

    exemplo percorrendo array

---
+++ Colecoes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            int[] arrayInt = new int[3];
+            arrayInt[0] = 10;
+            arrayInt[1] = 5;
+            arrayInt[2] = 6;
+
+            System.Console.WriteLine("Percorrendo array");
+            
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                System.Console.WriteLine(arrayInt[i]);
+            }
+        }
+         
+    }
+}
